7984d2e34f5d79d92f9d958f812596b5cb573aea
KarenCortesH <karendahcortes@gmail.com>
2024-11-26 23:34:17 -0500

    feat: update frontend

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const usuariosRoutes = require('./routes/usuarios');
 const inventariosRoutes = require('./routes/inventarios');
 const registroInventariosRoutes = require('./routes/registroInventarios');
 const sequelize = require('./config/database');
 
+// Crear la aplicación de Express
 app.use(express.json());
+
+// Habilitar CORS para todas las rutas y métodos
+app.use(cors()); 
 //api usuarios
 app.use('/api/usuarios', usuariosRoutes);
 
--- routes/inventarios.js
+++ routes/inventarios.js
@@ -1,46 +1,51 @@
-'use strict';
-const express = require('express');
-const { Inventario } = require('../models'); // Asegúrate de tener el modelo configurado correctamente
+"use strict";
+const express = require("express");
+const { Inventario } = require("../models"); // Asegúrate de tener el modelo configurado correctamente
 const router = express.Router();
 
 // Listar todos los inventarios
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const inventarios = await Inventario.findAll();
     res.status(200).json(inventarios);
   } catch (error) {
-    console.error('Error al listar inventarios:', error);
-    res.status(500).json({ error: 'Error al listar inventarios' });
+    console.error("Error al listar inventarios:", error);
+    res.status(500).json({ error: "Error al listar inventarios" });
   }
 });
 
 // Obtener un inventario por ID
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const inventario = await Inventario.findByPk(req.params.id);
     if (!inventario) {
-      return res.status(404).json({ error: 'Inventario no encontrado' });
+      return res.status(404).json({ error: "Inventario no encontrado" });
     }
     res.status(200).json(inventario);
   } catch (error) {
-    console.error('Error al obtener inventario:', error);
-    res.status(500).json({ error: 'Error al obtener inventario' });
+    console.error("Error al obtener inventario:", error);
+    res.status(500).json({ error: "Error al obtener inventario" });
   }
 });
 
 // Crear un inventario
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { producto, lote, cantidad, estado } = req.body;
-    const nuevoInventario = await Inventario.create({ producto, lote, cantidad, estado });
+    const nuevoInventario = await Inventario.create({
+      producto,
+      lote,
+      cantidad,
+      estado,
+    });
     res.status(201).json(nuevoInventario);
   } catch (error) {
-    console.error('Error al crear inventario:', error);
-    res.status(500).json({ error: 'Error al crear inventario' });
+    console.error("Error al crear inventario:", error);
+    res.status(500).json({ error: "Error al crear inventario" });
   }
 });
 
-// Actualizar un inventario
+//Actualizar un inventario
 router.put('/:id', async (req, res) => {
   try {
     const { id } = req.params;
@@ -59,21 +64,49 @@ router.put('/:id', async (req, res) => {
   }
 });
 
+// Actualizar un inventario
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params; // Obtenemos el ID del inventario desde la URL
+//     const { producto, lote, cantidad, estado } = req.body; // Obtenemos los datos del cuerpo de la solicitud
+
+//     // Buscar el inventario por ID
+//     const inventario = await Inventario.findByPk(id);
+//     if (!inventario) {
+//       return res.status(404).json({ error: "Inventario no encontrado" });
+//     }
+
+//     // Actualizamos los datos del inventario
+//     const inventarioActualizado = await inventario.update({
+//       producto,
+//       lote,
+//       cantidad,
+//       estado,
+//     });
+
+//     // Retornamos la respuesta con los datos actualizados
+//     res.status(200).json(inventarioActualizado);
+//   } catch (error) {
+//     console.error("Error al actualizar inventario:", error);
+//     res.status(500).json({ error: "Error al actualizar inventario" });
+//   }
+// });
+
 // Eliminar un inventario
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
 
     const inventario = await Inventario.findByPk(id);
     if (!inventario) {
-      return res.status(404).json({ error: 'Inventario no encontrado' });
+      return res.status(404).json({ error: "Inventario no encontrado" });
     }
 
     await inventario.destroy();
-    res.status(200).json({ message: 'Inventario eliminado' });
+    res.status(200).json({ message: "Inventario eliminado" });
   } catch (error) {
-    console.error('Error al eliminar inventario:', error);
-    res.status(500).json({ error: 'Error al eliminar inventario' });
+    console.error("Error al eliminar inventario:", error);
+    res.status(500).json({ error: "Error al eliminar inventario" });
   }
 });
 
